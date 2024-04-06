841cd9758d66e730ee595fd44c17f7969e06f85c
Richalbert <richalbert@free.fr>
2024-04-06 08:09:05 +0200

    modif sur .gitignore exlure les exe

--- chap-01/2-structures-de-controles/ex1.cpp
+++ chap-01/2-structures-de-controles/ex1.cpp
@@ -9,7 +9,8 @@ int main()
     std::vector<int> tab = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
     int n = tab.size();
-    std::cout << n << std::endl;
+    //std::cout << n << std::endl;
+    
     for (int it = 0; it < n; ++it)
     {
         if (tab[it] % 2 == 0)
